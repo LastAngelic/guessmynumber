8ac6c3b61214d35a96a63eba9980979ac8f10cef
LastAngelic <angelpenagonzalez@hotmail.com>
2022-03-18 13:43:20 -0600

    Function calculateText

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,3 +1,4 @@
+import { type } from '@testing-library/user-event/dist/type';
 import React, { Component } from 'react';
 import './Game.css';
 
@@ -5,21 +6,47 @@ class Game extends Component {
     constructor() {
         super()
         this.state = {
-            number: 0,
+            number: "",
             message: "",
             random: generareRandomNumber(100)
         }
     }
+
+    handleOnChange = e => {
+        //const value = e.target.value}
+
+        const {target: {value}} = e;
+        
+        console.log(value);
+
+        if(value.trim() >= 0) {
+            this.setState({number: value});
+        }
+    }
+
+    handleOnClick = () => {
+        const number = parseInt(this.state.number);
+        const random = parseInt(this.state.random);
+
+        console.log(random);
+
+        const text = calculateText(number, random);
+
+        this.setState({message: text});
+    }
+
     render() {
+        //console.log(this.state.random)
         return (
             <div className="game">
                 <input
-                type="number"
-
+                type = "number"
+                value = {this.state.number}
+                onChange = {this.handleOnChange}
                 />
 
-                <button>Probar</button>
-                <p>Texto</p>
+                <button onClick={this.handleOnClick}>Probar</button>
+                <p>{this.state.message}</p>
             </div>
         );
     }
@@ -29,4 +56,18 @@ export default Game;
 
 function generareRandomNumber(max, min=1){
     return Math.floor(Math.random()*(max-min + min));
+}
+
+function calculateText(number, random) {
+    if(number=== random) {
+        return "Felicidades has acertado!"
+    }
+
+    if(number > random){
+        return "Tu numero es mayor"
+    }
+
+    if(number < random){
+        return "Tu numero es menor"
+    }
 }
